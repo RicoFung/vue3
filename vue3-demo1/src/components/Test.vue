429dbb84d6226acd7a534c9ed1fafff45e09d429
RicoFung <ricofungcn@icloud.com>
2022-05-10 21:49:27 +0800

    first commit

---
+++ vue3-demo1/src/components/Test.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  title: String
+})	
+
+// define value
+const message = 'Hello Vue!!'
+// defin func
+const tap = () => {
+	alert("tap")
+}
+</script>
+
+<template>
+	<h1>{{ title }}</h1>
+	<div id="hello-vue" class="demo">
+	  {{ message }}
+	</div>
+	<button type="button" @click="tap">Tap</button>
+</template>
+
+<style>
+</style>
